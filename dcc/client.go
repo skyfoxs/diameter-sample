2a05fa5d724eb3677e15d319e8f01428135c319a
Supakit Thanadittagorn <supakit@riverpark.co.th>
2015-05-21 22:23:33 +0700

    Add client send CER

--- dcc/client.go
+++ dcc/client.go
@@ -1,3 +1,84 @@
 package dcc
 
+import (
+	"net"
 
+	"github.com/fiorix/go-diameter/diam"
+	"github.com/fiorix/go-diameter/diam/avp"
+	"github.com/fiorix/go-diameter/diam/datatype"
+)
+
+type DiameterClient struct {
+	URL     string
+	conn    diam.Conn
+	handler *diam.ServeMux
+
+	errorCh chan error
+	cerCh   chan *diam.Message
+}
+
+func (d *DiameterClient) ErrorNotify() <-chan error {
+	return d.errorCh
+}
+
+func (d *DiameterClient) CERDoneNotify() <-chan *diam.Message {
+	return d.cerCh
+}
+
+func NewClient(address string) *DiameterClient {
+	client := &DiameterClient{
+		URL: address,
+
+		errorCh: make(chan error),
+		cerCh:   make(chan *diam.Message),
+	}
+	client.handler = diam.NewServeMux()
+	client.handler.Handle("CEA", client.HandleCEA())
+
+	return client
+}
+
+func (d *DiameterClient) Start() {
+	var err error
+	d.conn, err = diam.Dial(d.URL, d.handler, nil)
+	if err != nil {
+		d.errorCh <- err
+	}
+}
+
+func (d *DiameterClient) SendCER() {
+	var (
+		CapabilitiesExchange uint32 = 257
+		OriginHost                  = datatype.DiameterIdentity("jenkin13_OMR_TEST01")
+		OriginRealm                 = datatype.DiameterIdentity("dtac.co.th")
+		vendorID                    = datatype.Unsigned32(0)
+		productName                 = datatype.UTF8String("omr")
+		FirmwareRevision            = datatype.Unsigned32(1)
+	)
+
+	m := diam.NewRequest(CapabilitiesExchange, 0, nil)
+
+	m.NewAVP(avp.OriginHost, avp.Mbit, 0, OriginHost)
+	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, OriginRealm)
+
+	ip, _, _ := net.SplitHostPort(d.conn.LocalAddr().String())
+	m.NewAVP(avp.HostIPAddress, avp.Mbit, 0, datatype.Address(net.ParseIP(ip)))
+	m.NewAVP(avp.VendorID, avp.Mbit, 0, vendorID)
+	m.NewAVP(avp.ProductName, 0, 0, productName)
+	m.NewAVP(avp.OriginStateID, avp.Mbit, 0, datatype.Unsigned32(0))
+	m.NewAVP(avp.SupportedVendorID, avp.Mbit, 0, datatype.Unsigned32(0))
+	m.NewAVP(avp.AuthApplicationID, avp.Mbit, 0, datatype.Unsigned32(4))
+	m.NewAVP(avp.AcctApplicationID, avp.Mbit, 0, datatype.Unsigned32(4))
+	m.NewAVP(avp.FirmwareRevision, avp.Mbit, 0, FirmwareRevision)
+
+	_, err := m.WriteTo(d.conn)
+	if err != nil {
+		d.errorCh <- err
+	}
+}
+
+func (d *DiameterClient) HandleCEA() diam.HandlerFunc {
+	return func(conn diam.Conn, m *diam.Message) {
+		d.cerCh <- m
+	}
+}
--- dcc/client_test.go
+++ dcc/client_test.go
@@ -1,9 +1,73 @@
 package dcc
 
 import (
-    "testing"
+	"fmt"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/fiorix/go-diameter/diam"
+	"github.com/fiorix/go-diameter/diam/avp"
+	"github.com/fiorix/go-diameter/diam/datatype"
+	"github.com/fiorix/go-diameter/diam/diamtest"
 )
 
-func TestClientRequestCER(t *testing.T){
+type Server struct {
+	*diamtest.Server
 
+	errorCh chan error
+}
+
+func (s *Server) ErrorNotify() <-chan error {
+	return s.errorCh
+}
+
+func NewTestServer() *Server {
+	testServer := &Server{
+		errorCh: make(chan error),
+	}
+
+	smux := diam.NewServeMux()
+	smux.Handle("CER", testServer.HandleCER())
+
+	testServer.Server = diamtest.NewServer(smux, nil)
+
+	return testServer
+}
+
+func TestClientRequestCER(t *testing.T) {
+	server := NewTestServer()
+	defer server.Close()
+
+	client := NewClient(server.Address)
+	client.Start()
+	client.SendCER()
+
+	select {
+	case err := <-server.ErrorNotify():
+		t.Error(err)
+	case err := <-client.ErrorNotify():
+		t.Error(err)
+	case m := <-client.CERDoneNotify():
+		fmt.Println(m)
+	}
+}
+
+func (s *Server) HandleCER() diam.HandlerFunc {
+	return func(conn diam.Conn, m *diam.Message) {
+		answerMessage := m.Answer(diam.Success)
+		s.SendCEA(conn, answerMessage)
+	}
+}
+
+func (s *Server) SendCEA(w io.Writer, m *diam.Message) {
+	m.NewAVP(avp.OriginHost, avp.Mbit, 0, datatype.DiameterIdentity("srv"))
+	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, datatype.DiameterIdentity("localhost"))
+	m.NewAVP(avp.HostIPAddress, avp.Mbit, 0, datatype.Address(net.ParseIP("127.0.0.1")))
+	m.NewAVP(avp.VendorID, avp.Mbit, 0, datatype.Unsigned32(99))
+	m.NewAVP(avp.ProductName, avp.Mbit, 0, datatype.UTF8String("go-diameter"))
+	_, err := m.WriteTo(w)
+	if err != nil {
+		s.errorCh <- err
+	}
 }
